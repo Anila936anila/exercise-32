1db3645d433454c26d9d2a34a36759c14394d051
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 13:06:34 -0800

    Checking Username
    Typescript 45Q assignment exercise 32

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Checking usernames: 
+//Make a list of five or morenusernames called current user.
+// Step 1.
+const current_users = ["Anila", "Mahreen", "Sobia", "Rubab", "Asifa"];
+const new_users = ["Anil", "Mahr", "Sobi", "Rub", "AsIf"];
+";
+{
+    const lowercasedCurrentUsers = current_users.map(user => user.toLowerCase());
+    for (; ; )
+        ;
+}
